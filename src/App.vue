8f1f36cccbf429b22687bedaec1a39aaf7e5b315
helen439 <zhelen439@gmail.com>
2025-08-07 02:41:51 +1000

    chore: create folders for components, pages, and router

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+ <!-- <script scoped></script> -->
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <nav class="navbar">
+    <div class="logo">MelMove</div>
+    <ul class="nav-links">
+      <li><router-link to="/urban-trends">Urban Trends</router-link></li>
+      <li><router-link to="/live">Live</router-link></li>
+      <li><router-link to="/forecast">Forecast</router-link></li>
+      <li><router-link to="/history">History</router-link></li>
+    </ul>
+  </nav>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<!-- <style scoped></style> -->
